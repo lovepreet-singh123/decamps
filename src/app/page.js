4d4346c8f78e96ed76049a918a2a723618d6fdf6
Lovepreet singh <lovepreet.singh@antiersolutions.com>
2025-02-12 12:01:14 +0530

    udpate

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import { Col, Row } from "react-bootstrap";
 // import { getAchievements } from "./lib/posts";
 import "./page.scss";
 import DynamicSection from "./DynamicSection";
+import Achievements from "./Achievements";
 
 export default async function Home() {
     // const achievements = await getAchievements(); // Fetch achievements
 
     return (
         <div>
-            <DynamicSection />
+            <Achievements />
+            {/* <DynamicSection /> */}
         </div>
     );
 }
