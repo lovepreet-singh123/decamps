a1ffe8e7c109260f80054d18a98b2ea135265d7d
@manisha.rampal <manisha.rampal@antiersolutions.com>
2025-02-17 13:21:16 +0530

    feature: tokenization section design

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/common/footer/footer";
 import Header from "@/components/common/header/header";
 import "./page.scss";
+import Tokenization from "@/components/pages/homepage/tokenization/tokenization";
 
 export default async function Home() {
     return (
         <>
             <div className="home_layout">
                 <Header />
+                <Tokenization/>
                 <Footer />
             </div>
             {/* <Achievements />
