7465a4d976674e924f02727e7d13589c8ec49834
ankush047 <ankush.ant@antiersolutions.com>
2025-02-17 16:23:56 +0530

    feature: get touch section added

---
+++ public/assets/icons/icons.js
@@ -0,0 +1,11 @@
+export const ArrowIcon = () =>
+    <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M1.19922 6.75453H14.7174M8.80319 1.31836L14.7174 6.75453L8.80319 12.1907"
+            stroke="white"
+            strokeWidth="1.55319"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/common/footer/footer";
 import Header from "@/components/common/header/header";
 import "./page.scss";
+import GetTouch from "@/components/pages/homepage/get-touch/get-touch";
 
 export default async function Home() {
     return (
         <>
             <div className="home_layout">
                 <Header />
+                <GetTouch/>
                 <Footer />
             </div>
             {/* <Achievements />
